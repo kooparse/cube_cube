85e4dfffe75973e3e49a113bd72b686cf3786c71
Alexandre Chêne <kooparse@protonmail.com>
2020-03-07 13:06:51 +0100

    That's one small step for a man

---
+++ Assets/Game/Scripts/Init.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+     [RuntimeInitializeOnLoadMethod]
+    static void Hey()
+    {
+        Debug.Log("Hey");
+    }
+}
